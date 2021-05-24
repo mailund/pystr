dd6de44fc9ec8022fe2cf19e19fd94c755ba9942
Thomas Mailund <mailund@birc.au.dk>
2021-05-24 07:42:01 +0200

    breadth first trie

--- pystr/trie.py
+++ pystr/trie.py
@@ -1,17 +1,28 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
-from typing import Optional
+from collections import defaultdict
+from typing import Optional, TypeVar, NamedTuple
+from .subseq import subseq
 
 
-@dataclass
+T = TypeVar("T")
+S = TypeVar("S")
+
+
+@dataclass(eq=False)
 class TrieNode:
     label: Optional[int] = None
-    out: dict[str, TrieNode] = field(default_factory=dict, init=False)
+    out: dict[str, TrieNode] = field(default_factory=dict)
+
+    def __eq__(self, other) -> bool:
+        return type(other) == type(self) and \
+            sorted(self.out) == sorted(other.out) and \
+            all(self.out[k] == other.out[k] for k in self.out)
 
 
-@dataclass
+@dataclass(eq=False)
 class Trie:
-    root: TrieNode = field(default_factory=TrieNode, init=False)
+    root: TrieNode = field(default_factory=TrieNode)
 
     def insert(self, x: str, label: int):
         n = self.root
@@ -28,3 +39,37 @@ class Trie:
                 return False
             n = n.out[x[i]]
         return n.label is not None
+
+    def __eq__(self, other) -> bool:
+        return type(other) == type(self) and self.root == other.root
+
+
+def depth_first_trie(*strings: str) -> Trie:
+    """The simplest construction just insert one string at a time."""
+    trie = Trie()
+    for i, x in enumerate(strings):
+        trie.insert(x, i)
+    return trie
+
+
+# Labelled strings...
+LS = NamedTuple('LS', [('label', int), ('x', subseq[str])])
+
+
+def breadth_first_trie_rec(strings: list[LS]) -> TrieNode:
+    empty = [label for label, x in strings if len(x) == 0]
+    assert len(empty) <= 1
+    node = TrieNode(label=(empty[0] if len(empty) == 1 else None))
+
+    non_empty = [LS(label, x) for label, x in strings if len(x) != 0]
+    out_groups = defaultdict(list)
+    for label, x in non_empty:
+        out_groups[x[0]].append(LS(label, x[1:]))
+    for k in out_groups:
+        node.out[k] = breadth_first_trie_rec(out_groups[k])
+    return node
+
+
+def breadth_first_trie(*strings: str) -> Trie:
+    labelled = list(LS(i, subseq[str](x)) for i, x in enumerate(strings))
+    return Trie(breadth_first_trie_rec(labelled))
--- tests/trie_test.py
+++ tests/trie_test.py
@@ -1,4 +1,6 @@
 from pystr.trie import Trie
+from pystr.trie import depth_first_trie
+from pystr.trie import breadth_first_trie
 
 
 def test_simple_trie():
@@ -7,8 +9,36 @@ def test_simple_trie():
     trie.insert("bar", 1)
     trie.insert("foobar", 2)
 
+    assert trie == trie
+
     assert "foo" in trie
     assert "foobar" in trie
     assert "bar" in trie
     assert "fo" not in trie
     assert "baz" not in trie
+
+
+def test_depth_first_trie():
+    trie = Trie()
+    trie.insert("foo", 0)
+    trie.insert("bar", 1)
+    trie.insert("foobar", 2)
+
+    assert trie == depth_first_trie("foo", "bar", "foobar")
+
+
+def test_breadth_first_trie():
+    trie = Trie()
+    trie.insert("foo", 0)
+    trie.insert("bar", 1)
+    trie.insert("foobar", 2)
+
+    assert trie == breadth_first_trie("foo", "bar", "foobar")
+
+
+if __name__ == '__main__':
+    globs = list(globals().items())
+    for name, f in globs:
+        if name.startswith("test_"):
+            print(name)
+            f()
